60c9d72710b83b5c7174d09bbc20b0503bf82e89
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2022-06-03 19:20:18 -0400

    selllelelelelct

--- hello-world/createSelect.jsx
+++ hello-world/createSelect.jsx
@@ -1,5 +1,19 @@
 import { Select } from "./WPMODULES";
 
 export function createSelect() {
-    return Select({})
+    /*{
+    "className": "sort-37NWdz",
+    "popoutClassName": "sortPopout-zN4OCq",
+    "options": [
+        {
+            "value": "Last Message",
+            "label": "Recent Activity"
+        },
+        {
+            "value": "Creation",
+            "label": "Creation Time"
+        }
+    ]
+    }*/
+    return Select({ options: [{ "value": "Hello", "label": "World" }] });
 }
