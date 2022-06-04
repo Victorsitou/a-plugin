3329416b8362438b24b534d45c83efc802bdd1a0
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2022-06-03 21:03:42 -0400

    asdsadsa

---
+++ hello-world/avatarDecorations.js
@@ -0,0 +1,88 @@
+export function getAvatarDecorations() {
+    return [
+        {
+            "name": "None",
+            "url": null
+        },
+        {
+            "name": "BearHood",
+            "url": "https://canary.discord.com/assets/84508a07ebb0533ea77ac03e71d9d38c.png"
+        },
+        {
+            "name": "Egg",
+            "url": "https://canary.discord.com/assets/df029aed54f08f780a1c305fff8b9183.svg"
+        },
+        {
+            "name": "EmoHair",
+            "url": "https://canary.discord.com/assets/a8b3efde89d07099e08549a72e7eb489.png"
+        },
+        {
+            "name": "FloppyCat",
+            "url": "https://canary.discord.com/assets/ed315fe1999b1889a94c8473f5d53d5d.svg"
+        },
+        {
+            "name": "Turtle",
+            "url": "https://canary.discord.com/assets/adea5750166b2879746170a7551f7740.svg"
+        },
+        {
+            "name": "BiteyDog",
+            "url": "https://canary.discord.com/assets/19dd1ba662834eab6d946deee868ed0c.svg"
+        },
+        {
+            "name": "Frog",
+            "url": "https://canary.discord.com/assets/ab616dac9329b540438693b3086eb399.svg"
+        },
+        {
+            "name": "SnakeHelmet",
+            "url": "https://canary.discord.com/assets/d11711be026dff82140d7bb048325e20.png"
+        },
+        {
+            "name": "CandyPlanet",
+            "url": "https://canary.discord.com/assets/f16649030c7d975c74e4794e53975a13.png"
+        },
+        {
+            "name": "Neon",
+            "url": "https://canary.discord.com/assets/3e86ad03bf042a6a02a7c9b04253f03b.svg"
+        },
+        {
+            "name": "Rainbow",
+            "url": "https://canary.discord.com/assets/970f1f3a88a7604624ea26ca0481132b.svg"
+        },
+        {
+            "name": "StickyHands",
+            "url": "https://canary.discord.com/assets/9dffa4c700d55f2bff06e44932a52454.png"
+        },
+        {
+            "name": "BarbedWire",
+            "url": "https://canary.discord.com/assets/3dc7fa9fb808a237cb0330ad0cd79a97.png"
+        },
+        {
+            "name": "Fire",
+            "url": "https://canary.discord.com/assets/0123fbcf1358c97c1f8b851e6563a9f0.png"
+        },
+        {
+            "name": "Lightning",
+            "url": "https://canary.discord.com/assets/1d57c1a5ed723c7e695196b14eee3c2c.png"
+        },
+        {
+            "name": "Neon2",
+            "url": "https://canary.discord.com/assets/26c56e301889a9ec94c159ce07bf72a3.png"
+        },
+        {
+            "name": "Snake",
+            "url": "https://canary.discord.com/assets/055671257e4fd25fba12cfca6a49b313.png"
+        },
+        {
+            "name": "StringLights",
+            "url": "https://canary.discord.com/assets/08dd1a481bbdef42bd5563f95774f3b0.png"
+        },
+        {
+            "name": "HotDog",
+            "url": "https://canary.discord.com/assets/ff038bed535f1e15e4292e030600b7f4.svg"
+        },
+        {
+            "name": "PC",
+            "url": "https://canary.discord.com/assets/3d73f3c6bbbc149de9db4713abd7a284.svg"
+        }
+    ]
+}
--- hello-world/createSelect.jsx
+++ hello-world/createSelect.jsx
@@ -1,8 +1,18 @@
 import { Select } from "./WPMODULES";
+import { getAvatarDecorations } from "./avatarDecorations"
 
 function onChange_(e) {
     console.log(e);
     return e;
+}
+
+function createOptions() {
+    data = getAvatarDecorations();
+    let options = [];
+    data.forEach(function (item) {
+        options.push({ value: item.url, label: item.name });
+    });
+    return options;
 }
 
 export function createSelect() {
@@ -22,7 +32,7 @@ export function createSelect() {
     }*/
     return Select.SingleSelect(
         {
-            options: [{ "value": "asd", "label": "fgh" }, { "value": "qwe", "label": "erty" }],
+            options: createOptions(),
             value: "asd",
             onChange: onChange_
         }
