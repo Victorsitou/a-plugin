c09b6f233c8002cc461f6df13eebba34299d7b83
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2022-06-03 21:30:06 -0400

    ?

--- hello-world/createSelect.jsx
+++ hello-world/createSelect.jsx
@@ -1,7 +1,24 @@
 import { Select } from "./WPMODULES";
 import { getAvatarDecorations } from "./avatarDecorations"
+import { instead } from "@cumcord/patcher"
+import { findByPropsAll, findByProps } from "@cumcord/modules/webpack"
+const { currentUser } = findByProps("getCurrentUser").getCurrentUser;
 
 function onChange_(e) {
+    instead("getAvatarDecorationURL", findByPropsAll("getAvatarDecorationURL")[1], (args) => {
+        if (args[0].userId == currentUser().id) {
+            return "https://canary.discord.com/assets/ed315fe1999b1889a94c8473f5d53d5d.svg"
+        }
+        var e = args[0]
+        var t = e.userId
+        var n = e.avatarDecoration
+        var r = e.size;
+        if (null != n) {
+            var i;
+            i = "https://cdn.discordapp.com/avatar-decorations/" + t + "/" + n + ".png?size=" + r
+            return i
+        }
+    });
     console.log(e);
     return e;
 }
